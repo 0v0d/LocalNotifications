6eac12d67739b06c7fc44f2704b498f429dc39bf
0v0d <legolesia@gmail.com>
2024-12-28 13:12:07 +0900

    Initial Commit

---
+++ LocalNotifications/LocalNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationsApp.swift
+//  LocalNotifications
+//
+//  Created by 0v0 on 2024/12/28.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
